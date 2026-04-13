3afb2b222332678578a3da0087e9a0a5404cd557
Tieswheringa <tieswheringa@gmail.com>
2026-04-13 16:36:41 +0200

    test chat link (3opdr)

--- scrape_prototype.py
+++ scrape_prototype.py
@@ -395,7 +395,7 @@ def run_scraper(credentials: dict, drempel: int, log_fn, progress_fn, result_fn)
 
         alle_matches = []
 
-        for i, url in enumerate(alle_urls[:1]):
+        for i, url in enumerate(alle_urls[:3]):
             progress_fn(i + 1, len(alle_urls))
             log(f"\n[{i+1}/{len(alle_urls)}] {url}")
 
